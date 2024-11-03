e5ca29713e932d7448632cab654ea95907bc4e18
Shashidharak89 <shashidharak334@gmail.com>
2024-11-03 16:26:01 +0530

    preloader

---
+++ javascript/preloader.js
@@ -0,0 +1,6 @@
+
+// JavaScript to hide preloader after the page loads
+window.addEventListener("load", function () {
+    const preloader = document.getElementById("preloader");
+    preloader.classList.add("hidden");
+});
